2f23272bf3605d6bdd465acf6d57334292b0ed58
Jxstxsh <justus.haensel@mailbox.tu-dresden.de>
2025-11-13 12:51:57 +0100

    dots get color according to engineSize

--- main.js
+++ main.js
@@ -8,25 +8,32 @@ window.onload = () => {
 	const height = +svg.attr("height");
 	const margin = { top: 30, right: 30, bottom: 50, left: 80 };
 
+	function EngineSizeToColor(engineSize) {
+		if (engineSize < 1.4) {
+			return "grey"
+		} else if (engineSize < 2.0) {
+			return "lightblue"
+		} else if (engineSize < 3.0) {
+			return "blue"
+		} else if (engineSize < 4.0) {
+			return "green"
+		} else if (engineSize < 5.0) {
+			return "orange"
+		} else {
+			return "red"
+		}
+	}
+
 	// Load the data set from the assets folder:
 	d3.csv("cars.csv").then(rawData => {
 		const data = rawData.map(d => ({
 		x: +d.Horsepower,
 		y: +d.RetailPrice,
-		//engineSize: +d.EngineSize,
+		engineSize: +d.EngineSize,
 		//type: +d.Type
 		}));
 		return data;
-	}).then(data => {
-
-
-	//d3.csv("example.csv").then(rawData => {
-		//const data = rawData.map(d => ({
-			//x: +d.x,
-			//y: +d.y
-		//}));
-		//return data;
-	//}).then(data => {
+	}).then(data => {	
 
 	// Define scales
 	const xScale = d3.scaleLinear()
@@ -36,6 +43,10 @@ window.onload = () => {
 	const yScale = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.y)])
     .range([height - margin.bottom, margin.top]);
+
+	const scaleOrdinalEngine = d3.scaleOrdinal()
+	.domain([1.4, 6.0])  // Beispielwerte für Motorgrößen
+	.range(d3.schemeCategory10);  // Verwenden eines vordefinierten Farbschemas
 
 	// Add axes
 	const xAxis = d3.axisBottom(xScale);
@@ -55,7 +66,8 @@ window.onload = () => {
     .append("circle")
     .attr("cx", d => xScale(d.x))
     .attr("cy", d => yScale(d.y))
-    .attr("r", 6);
+    .attr("r", 6)
+    .attr("fill", d => EngineSizeToColor(d.engineSize));
 
 	// label for x axis
 	svg.append("text")
@@ -76,8 +88,8 @@ window.onload = () => {
 	});
 
 	// Attributes and their encoding
-	// Color ->   Engine Size (l)
-	// Symbol ->  Type
-	// X-Axis ->  Horsepower
-	// Y-Axis ->  Retail price
+	// Color ->   Engine Size (l) 		-> from 0 to 6 (1.4 is the lowest actual value)
+	// Symbol ->  Type od car			-> Sedan, SUV, Sports Car, Wagon, Minivan
+	// X-Axis ->  Horsepower			-> from 73 to 493
+	// Y-Axis ->  Retail price			-> from 10.280 to 192.465
 };
